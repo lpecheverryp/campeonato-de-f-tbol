fa77986e23d742053c5c0af4fabe7f635940d2b1
Estudiantes <Estudiantes@Labing50606.udistrital.edu.co>
2017-03-08 17:28:03 -0500

    crear todas las clases y cree objetos en clase fanático

---
+++ Arreglos/src/arreglos/Fanático.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Fanático {
+    String nombre;
+    int edad;
+    float gradoFanatismo;
+    int PuñaladasRecibidas;
+    void gritar (){
+    }
+    
+    void apuñalar(){
+        
+    }
+    void Madrear(){
+        
+    }
+    
+    void Limosnear(){
+        
+    }
+    }
+            
+
